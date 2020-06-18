cab13cd9034f3802fd6aa4bce0387f0292cc5afa
Modijenil <mjenild5@gmail.com>
2020-06-18 13:29:07 +0530

    Create DateUtil.java

---
+++ Case_Study/src/com.util/DateUtil.java
@@ -0,0 +1,17 @@
+package com.util;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class DateUtil {
+ public static Date convertStringToDate(String dateInString,String format) {
+	SimpleDateFormat formatter=new SimpleDateFormat(format); 
+	Date date=null;
+	try {
+		System.out.println("Date String:"+dateInString);
+		date=formatter.parse(dateInString);
+	}catch (ParseException e) {
+		e.printStackTrace();
+	}
+	return date;
+ }
+}
